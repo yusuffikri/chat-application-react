6060cabaf4d4933c0c36ecdbb227e5701655ec2a
yusuffikri <yusuffiks99@gmail.com>
2023-03-09 21:12:31 +0800

    react chat engine setup

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow} from 'react-chat-engine-advanced';
 
 const Chat = () => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "testuser",
+        "1234"
+    )
+
   return (
-    <p>Chatqweqweasd</p>
+    <div style={{ flexBasis: "100%" }}>
+        <MultiChatSocket {...chatProps}/>
+        <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat)=> <Header chat={chat}/> }
+        />
+    </div>
   )
 }
 
