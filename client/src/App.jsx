fb029d1cd379a08250c948f8fc868e2eb4afc4c4
yusuffikri <yusuffiks99@gmail.com>
2023-03-09 20:25:56 +0800

    main chat update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import 
+
 function app() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="app">
       <div>
-        app
+        <BrowserRouter>
+        <Routes>
+        <Route path="/chat" element={<Chat/>}></Route>
+        </Routes>
+        </BrowserRouter>
       </div>
     </div>
   )
