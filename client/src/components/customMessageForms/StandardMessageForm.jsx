c28f6616e05d0dc9394ca5b15c839070e8b6bbfd
yusuffikri <yusuffiks99@gmail.com>
2023-03-11 00:22:03 +0800

    add time for message

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -10,8 +10,20 @@ const StandardMessageForm = ({props, activeChat}) => {
     const handleChange = (e) => setMessage(e.target.value);
 
     const handleSubmit = async () => {
-        const date = new Date().toISOString().replace("T", " ").replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`)
-    }
+        const date = new Date().toISOString().replace("T", " ").replace("Z", `${Math.floor(Math.random() * 1000)}-08:00`);
+        const at =  attachment ? [{blob : attachment, file: attachment.name}]:[];
+        const form = {
+            attachments: at,
+            created: date,
+            sender_username: props.username,
+            text: message,
+            activeChatId: activeChat.id,
+        }
+
+        props.onSubmit(form);
+        setMessage("");
+        setAttachment(""); 
+    };
 
   return (
     <div className='message-form-container'>
