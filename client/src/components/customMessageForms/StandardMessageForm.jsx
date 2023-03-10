3acad944ea28acdc52830406197d60baaf9d97f8
yusuffikri <yusuffiks99@gmail.com>
2023-03-10 23:53:45 +0800

    add time for message

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
-import XMarkIcon from '@heroicons/react/24/solid'
-const StandardMessageForm = () => {
+import XMarkIcon, { PaperAirplaneIcon, PaperClipIcon } from '@heroicons/react/24/solid'
+import Dropzone from 'react-dropzone';
+
+const StandardMessageForm = ({props, activeChat}) => {
     const [message, setMessage] = useState("");
     const [attachment, setAttachment] = useState("");
     const [preview, setPreview] = useState("");
 
     const handleChange = (e) => setMessage(e.target.value);
+
+    const handleSubmit = async () => {
+        const date = new Date().toISOString().replace("T", " ").replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`)
+    }
 
   return (
     <div className='message-form-container'>
@@ -20,7 +26,7 @@ const StandardMessageForm = () => {
                 <input type="text" className='message-form-input' value={message} onChange={handleChange} placeholder='Send a message...' />
            </div>
            <div className='message-form-icons'>
-            <Dropzone acceptedFiles='.jpg,.jpeg,.png' multiple={false} noClick={true}
+            <Dropzone acceptedFiles=".jpg,.jpeg,.png" multiple={false} noClick={true}
             onDrop={(acceptedFiles) => {
                 setAttachment(acceptedFiles[0]);
                 setPreview(URL.createObjectURL(acceptedFiles));
@@ -28,9 +34,17 @@ const StandardMessageForm = () => {
             {({getRootProps,getInputProps, open}) => (
                 <div {...getRootProps()}>
                     <input {...getInputProps()} />
+                    <PaperClipIcon className='message-form-icon-clip' onClick={open}/>
                 </div>
             )}
             </Dropzone>
+
+            <hr className='vertical-line'/>
+            <p><PaperAirplaneIcon className='message-form-icon-airplane' onClick={() =>  {
+                setPreview("");
+                handleSubmit(); 
+            }}/>
+             </p>
            </div>
         </div>
     </div>
