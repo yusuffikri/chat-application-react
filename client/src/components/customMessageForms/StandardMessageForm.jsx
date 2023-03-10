8790a2eed8c111a28fe165fbc01d4e666825e628
yusuffikri <yusuffiks99@gmail.com>
2023-03-10 19:09:47 +0800

    update image preview of message form

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import XMarkIcon from '@heroicons/react/24/solid'
 const StandardMessageForm = () => {
     const [message, setMessage] = useState("");
     const [attachment, setAttachment] = useState("");
@@ -9,9 +9,13 @@ const StandardMessageForm = () => {
     <div className='message-form-container'>
         { preview && (
             <div className='message-form-preview'>
-                <img src={preview} className='message-form-preview-image' onLoad={() => URL.revokeObjectURL(preview)} />
+                <img alt="message-form-preview"src={preview} className='message-form-preview-image' onLoad={() => URL.revokeObjectURL(preview)} />
+                <XMarkIcon className='message-form-icon-x' onClick={() => { setPreview(""); setAttachment("");}}/>
             </div>
         )}
+        <div>
+            asdfasf
+        </div>
     </div>
   )
 }
