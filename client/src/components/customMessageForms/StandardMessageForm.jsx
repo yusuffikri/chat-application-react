053069f4c3721b5af0461044a3ee12f97bb893f6
yusuffikri <yusuffiks99@gmail.com>
2023-03-10 19:02:33 +0800

    update image preview of message form

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StandardMessageForm = () => {
+    const [message, setMessage] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const [preview, setPreview] = useState("");
+
   return (
-    <div>StandardMessageForm</div>
+    <div className='message-form-container'>
+        { preview && (
+            <div className='message-form-preview'>
+                <img src={preview} className='message-form-preview-image' onLoad={() => URL.revokeObjectURL(preview)} />
+            </div>
+        )}
+    </div>
   )
 }
 
