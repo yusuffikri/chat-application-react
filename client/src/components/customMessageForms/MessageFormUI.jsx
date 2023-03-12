8424008520190f046ef043a95a6cfb7933970959
yusuffikri <yusuffiks99@gmail.com>
2023-03-12 17:26:18 +0800

    message form UI update

---
+++ client/src/components/customMessageForms/MessageFormUI.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import React, { useState } from 'react'
+import XMarkIcon, { PaperAirplaneIcon, PaperClipIcon } from '@heroicons/react/24/solid'
+import Dropzone from 'react-dropzone';
+
+const MessageFormUI = () => {
+  return (
+    <div className='message-form-container'>
+        { preview && (
+            <div className='message-form-preview'>
+                <img alt="message-form-preview"src={preview} className='message-form-preview-image' onLoad={() => URL.revokeObjectURL(preview)} />
+                <XMarkIcon className='message-form-icon-x' onClick={() => { setPreview(""); setAttachment("");}}/>
+            </div>
+        )}
+        <div className='message-form'>
+           <div className='message-form-input-container'>
+                <input type="text" className='message-form-input' value={message} onChange={handleChange} placeholder='Send a message...' />
+           </div>
+           <div className='message-form-icons'>
+            <Dropzone acceptedFiles=".jpg,.jpeg,.png" multiple={false} noClick={true}
+            onDrop={(acceptedFiles) => {
+                setAttachment(acceptedFiles[0]);
+                setPreview(URL.createObjectURL(acceptedFiles));
+            }}>
+            {({getRootProps,getInputProps, open}) => (
+                <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <PaperClipIcon className='message-form-icon-clip' onClick={open}/>
+                </div>
+            )}
+            </Dropzone>
+
+            <hr className='vertical-line'/>
+            <p><PaperAirplaneIcon className='message-form-icon-airplane' onClick={() =>  {
+                setPreview("");
+                handleSubmit(); 
+            }}/>
+             </p>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default MessageFormUI
