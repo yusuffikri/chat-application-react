2d8cd9e04588423f25f24894866eecbd5a41a31c
yusuffikri <yusuffiks99@gmail.com>
2023-03-24 03:48:54 +0800

    chat header update

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
-import { useMultiChatLogic, MultiChatSocket, MultiChatWindow} from 'react-chat-engine-advanced';
-import Header from '@/components/customHeader'
-import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm'
-import Ai from '@/components/customMessageForms/Ai.jsx'
-import AiCode from '@/components/customMessageForms/AiCode.jsx'
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
+import Ai from "@/components/customMessageForms/Ai";
+import AiCode from "@/components/customMessageForms/AiCode";
+import AiAssist from "@/components/customMessageForms/AiAssist";
 
-const Chat = () => {
-    const chatProps = useMultiChatLogic(
-        import.meta.env.VITE_PROJECT_ID,
-        "testuser",
-        "1234"
-    )
+const Chat = ({ user, secret }) => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    user,
+    secret
+  );
 
   return (
     <div style={{ flexBasis: "100%" }}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
         {...chatProps}
         style={{ height: "100vh" }}
-        renderChatHeader={(chat)=> <Header chat={chat}/> }
+        renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
-          if (chatProps.chat?.title.startsWith("AiChat_")) {
-            return <Ai props={props} activeChat={chatProps.chat} />
+          if (chatProps.chat?.title.startsWith("Ai-Naomi")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
           }
-          if (chatProps.chat?.title.startsWith("AiCode_")) {
-            return <AiCode props={props} activeChat={chatProps.chat} />
+          if (chatProps.chat?.title.startsWith("Ai-Steven")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
+          }
+          if (chatProps.chat?.title.startsWith("AiAssist_")) {
+            return <AiAssist props={props} activeChat={chatProps.chat} />;
           }
 
           return (
-            <StandardMessageForm props={props} activeChat={chatProps.chat}/>
-          )
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
         }}
-        />
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Chat;
--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { ChatBubbleLeftRightIcon, PhoneIcon} from '@heroicons/react/24/solid'
+import React from "react";
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
-const CustomerHeader = ({chat}) => {
+const CustomerHeader = ({ chat }) => {
   return (
-    <div className='chat-header'>
-        <div className='flexbetween'>
-            <ChatBubbleLeftRightIcon className='icon-chat'/>
-            <h3 className="header-text">{chat.title}</h3>
-        </div>
-        <div>
-            <PhoneIcon className='icon-phone'/>
-            {chat.description !== "⬅️ ⬅️ ⬅️" ? (<p className='header-text'>{chat.description}</p>) : (<p className='header-text'>No chat selected.</p>)}
-        </div>
+    <div className="chat-header">
+      <div className="flexbetween">
+        <ChatBubbleLeftRightIcon className="icon-chat" />
+        <h3 className="header-text">{chat.title}</h3>
+      </div>
+      <div className="flexbetween">
+        <PhoneIcon className="icon-phone" />
+        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
+          <p className="header-text">{chat.description}</p>
+        ) : (
+          <p className="header-text">no chat selected</p>
+        )}
+      </div>
     </div>
-  )
-} 
+  );
+};
 
-export default CustomerHeader
+export default CustomerHeader;
