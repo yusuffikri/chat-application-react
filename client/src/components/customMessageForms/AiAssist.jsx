f73a8e8c01d296547454cf9ccd32024a733e6cb0
yusuffikri <yusuffiks99@gmail.com>
2023-03-25 11:50:07 +0800

    login configuration

--- client/src/components/customMessageForms/AiAssist.jsx
+++ client/src/components/customMessageForms/AiAssist.jsx
@@ -1,4 +1,4 @@
-import { usePostAiAssistMutation } from "@/state/api";
+// import { usePostAiAssistMutation } from "@/state/api";
 import React, { useEffect, useState } from "react";
 import MessageFormUI from "./MessageFormUI";
 
